1f6f4afd412c474aa71c37d320b912402a424de0
ankitoptimus <ankitkr.gupta@optimusinfo.com>
2015-07-14 18:40:54 +0530

    Added images and names as well as implemented constraint to bound to screen size

--- tabbedApp/tabbedApp/FirstViewController.m
+++ tabbedApp/tabbedApp/FirstViewController.m
@@ -15,6 +15,7 @@
 @implementation FirstViewController
 {
     NSArray *names;                 // To get the table names
+    NSArray *images;                // To get images names
 }
 
 - (void)viewDidLoad
@@ -22,7 +23,9 @@
     [super viewDidLoad];
     
     // Initialize table data
-    names = [NSArray arrayWithObjects:@"Ankit", @"Menka", @"Abhishek Jha", @"Abhishek Gupta", @"Akshi Bansal", @"Naveta Grover", @"Darsheel", @"Mayank", @"Neha", @"Nikhil", @"Priyansha", @"Shreya", @"Vidushi", @"Ansari", @"Divyansh", @"Aashi", @"Apoorv", @"Adit", @"Aditya" ,nil];
+    names = [NSArray arrayWithObjects:@"Ankit", @"Menka", @"Abhishek Jha", @"Abhishek Gupta", @"Akshi Bansal", @"Naveta Grover", @"Darsheel", @"Mayank", @"Neha", @"Nikhil", @"Priyansha", @"Shreya", @"Vidushi", @"Ansari", @"Divyansh", @"Aashi", @"Apoorv", @"Adit", @"Aditya", nil];
+    
+    images = [NSArray arrayWithObjects:@"1.jpg", @"2.jpg", @"3.jpg", @"4.jpg", @"5.jpg", @"6.jpg", @"7.jpg", @"8.jpg", @"9.jpg", @"10.jpg", @"11.jpg", @"12.jpg", @"13.jpg", @"14.jpg", @"15.jpg", @"16.jpg", @"17.jpg", @"18.jpg", @"19.jpg", @"19.jpg", nil];
     
     
 }
@@ -50,6 +53,8 @@
     }
     
     cell.textLabel.text = [names objectAtIndex:indexPath.row];          //adding names to the rows
+    cell.imageView.image = [UIImage imageNamed:[images objectAtIndex:indexPath.row]]; // Adding images to rows
+
     
     return cell;
 }
