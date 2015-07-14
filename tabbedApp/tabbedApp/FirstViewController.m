34f95ba1e6088f0022908e0d1c8ff46b6504a207
ankitoptimus <ankitkr.gupta@optimusinfo.com>
2015-07-14 11:54:31 +0530

    Intitial state of the App along with two tabs

---
+++ tabbedApp/tabbedApp/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  tabbedApp
+//
+//  Created by optimusmac4 on 7/14/15.
+//  Copyright (c) 2015 optimusmac4. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
