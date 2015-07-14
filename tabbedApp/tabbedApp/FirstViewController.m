36f625a71c38553add11a5c14cd941c20fd4d71e
ankitoptimus <ankitkr.gupta@optimusinfo.com>
2015-07-14 17:37:42 +0530

    Added Hard bound array of name of size 20 to the table

--- tabbedApp/tabbedApp/FirstViewController.m
+++ tabbedApp/tabbedApp/FirstViewController.m
@@ -13,15 +13,48 @@
 @end
 
 @implementation FirstViewController
+{
+    NSArray *names;                 // To get the table names
+}
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Initialize table data
+    names = [NSArray arrayWithObjects:@"Ankit", @"Menka", @"Abhishek Jha", @"Abhishek Gupta", @"Akshi Bansal", @"Naveta Grover", @"Darsheel", @"Mayank", @"Neha", @"Nikhil", @"Priyansha", @"Shreya", @"Vidushi", @"Ansari", @"Divyansh", @"Aashi", @"Apoorv", @"Adit", @"Aditya" ,nil];
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];    // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [names count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *TableIdentifier = @"NameCells";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:TableIdentifier];
+    
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:TableIdentifier];
+    }
+    
+    cell.textLabel.text = [names objectAtIndex:indexPath.row];          //adding names to the rows
+    
+    return cell;
+}
+
+
 
 @end
+
